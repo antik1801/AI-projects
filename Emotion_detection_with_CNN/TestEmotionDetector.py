c817b9ef6f816454f3d9a3b69e8dcd10697ebe72
antik1801 <antik.edu@gmail.com>
2022-08-05 22:59:44 +0600

    -update8-5-22

--- Emotion_detection_with_CNN/TestEmotionDetector.py
+++ Emotion_detection_with_CNN/TestEmotionDetector.py
@@ -3,10 +3,11 @@ import numpy as np
 from keras.models import model_from_json
 
 
-emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful",
+                3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 # load json and create model
-json_file = open('model/emotion_model.json', 'r')
+json_file = open('D:\AIUB\spring 21-22\cvpr\Emotion Detection\Emotion_detection_with_CNN\model', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 emotion_model = model_from_json(loaded_model_json)
@@ -16,11 +17,11 @@ emotion_model.load_weights("model/emotion_model.h5")
 print("Loaded model from disk")
 
 # start the webcam feed
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 # pass here your video path
 # you may download one from here : https://www.pexels.com/video/three-girls-laughing-5273028/
-cap = cv2.VideoCapture("C:\\JustDoIt\\ML\\Sample_videos\\emotion_sample6.mp4") # This is the  video file of our test dataset
+# cap = cv2.VideoCapture("C:\\JustDoIt\\ML\\Sample_videos\\emotion_sample6.mp4") # This is the  video file of our test dataset
 
 while True:
     # Find haar cascade to draw bounding box around face
@@ -28,22 +29,26 @@ while True:
     frame = cv2.resize(frame, (1280, 720))
     if not ret:
         break
-    face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+    face_detector = cv2.CascadeClassifier(
+        'haarcascades/haarcascade_frontalface_default.xml')
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detect faces available on camera
-    num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+    num_faces = face_detector.detectMultiScale(
+        gray_frame, scaleFactor=1.3, minNeighbors=5)
 
     # take each face available on the camera and Preprocess it
     for (x, y, w, h) in num_faces:
         cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
         roi_gray_frame = gray_frame[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        cropped_img = np.expand_dims(np.expand_dims(
+            cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
 
         # predict the emotions
         emotion_prediction = emotion_model.predict(cropped_img)
         maxindex = int(np.argmax(emotion_prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('Emotion Detection', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
---
+++ Emotion_detection_with_CNN/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+tion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_mode
